70e5f7a56ad1f93bb722c67da985a6072daa519f
sadikatanisha <sadikatanisha@gmail.com>
2023-10-15 11:51:54 +0600

    favicon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,8 +18,8 @@ const Navbar = () => {
         initial={{ y: -100, x: "-50%", opacity: 0 }}
         animate={{ y: 0, x: "-50%", opacity: 1 }}
       ></motion.div>
-      <nav className="flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
-        <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-300 sm:w-[initial] sm:flex-nowrap sm:gap-5">
+      <nav className="flex fixed w-[22rem] top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
+        <ul className="flex  flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-300 sm:w-[initial] sm:flex-nowrap sm:gap-5">
           <motion.li
             className="h-3/4 flex items-center justify-center relative"
             initial={{ y: -100, x: "-50%", opacity: 0 }}
@@ -43,8 +43,20 @@ const Navbar = () => {
               // onSetInactive={this.handleSetInactive}
               ignoreCancelEvents={false}
               spyThrottle={500}
+              onClick={() => setActiveSection("Home")}
             >
               Home
+              {"Home" == activeSection && (
+                <motion.span
+                  className="bg-gray-100 rounded-full absolute inset-0 -z-10 dark:bg-gray-800"
+                  layoutId="activeSection"
+                  transition={{
+                    type: "spring",
+                    stiffness: 380,
+                    damping: 30,
+                  }}
+                ></motion.span>
+              )}
             </Link>
           </motion.li>
           <motion.li
@@ -67,6 +79,7 @@ const Navbar = () => {
               // onSetInactive={this.handleSetInactive}
               ignoreCancelEvents={false}
               spyThrottle={500}
+              onClick={() => setActiveSection("About")}
             >
               About
             </Link>
