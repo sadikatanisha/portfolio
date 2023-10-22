ce496f46b572125169ba8594d560c9c86d7d8b25
sadikatanisha <sadikatanisha@gmail.com>
2023-10-22 15:26:25 +0600

    fix:mobile navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,8 +15,8 @@ const Navbar = () => {
         initial={{ y: -100, x: "-50%", opacity: 0 }}
         animate={{ y: 0, x: "-50%", opacity: 1 }}
       ></motion.div>
-      <nav className="flex fixed w-[22rem] top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
-        <ul className="flex  flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-300 sm:w-[initial] sm:flex-nowrap sm:gap-5">
+      <nav className="flex fixed w-[20rem] top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial]  sm:w-[22rem] sm:py-0 px-3">
+        <ul className="flex  flex-wrap items-center justify-center gap-2 text-[1rem] font-medium text-gray-300 sm:w-[initial] sm:flex-nowrap sm:gap-5">
           {links.map((link) => (
             <motion.li
               className="h-3/4 flex items-center justify-center relative"
